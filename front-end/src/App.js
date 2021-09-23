dc786d166889043372c15493cce865d1a8d93836
jwlexi <alexisanchez@csumb.edu>
2021-09-23 11:33:33 -0700

    Modifying CSS

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import './css/Welcome.css';
 function App() {
   return (
-    <div className="topCard text-center">
+    <div className="landingPage text-center">
             <section className="text-light WelcomeCard1">
                 <h1> Wishlist </h1>
                 <h3 className="p-2"> Add gifts from any website. <br/> Share with anyone. </h3>
