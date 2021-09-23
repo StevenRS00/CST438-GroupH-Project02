928dbab2b0aa69a34169b001983d3d82a04fcf08
jwlexi <alexisanchez@csumb.edu>
2021-09-23 11:26:44 -0700

    starting landing page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './css/Welcome.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          this is a test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="topCard text-center">
+            <section className="text-light WelcomeCard1">
+                <h1> Wishlist </h1>
+                <h3 className="p-2"> Add gifts from any website. <br/> Share with anyone. </h3>
+                <p className="text-muted"> Available now </p> 
+                
+                <form action="/login">
+                    <button className="btn btn-primary">Log in</button>
+                </form>
+                
+                <span className="text-muted largeFont">or</span>
+                
+                <form action="/register">
+                    <button className="btn btn-primary">Register</button>
+                </form>
+            </section>
+        </div>
   );
 }
 
