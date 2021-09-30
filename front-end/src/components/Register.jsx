89d91bb7ab83e4a29e545d7da646f96f5dddb758
jwlexi <alexisanchez@csumb.edu>
2021-09-30 11:51:10 -0700

    Refactoring

--- front-end/src/components/Register.jsx
+++ front-end/src/components/Register.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import '../css/Register.css';
+
 function Register() {
   return (
-    <main>
-        <form method="POST" action="/register">
-            <section class="registerFormPage">
-                <p class="heading text-dark" align="center">Register</p>
-                <input type="username" class="form-control un" id="usernameInput" name="username" placeholder="Enter username"/>
-                <input type="password" class="form-control pass" id="passwordInput" name="password" placeholder="Enter password"/>
+    <div>
+      <form method="POST" action="/register">
+        <section class="registerFormPage">
 
-                <input type="text" class="form-control un" id="firstNameInput" name="firstName" placeholder="Enter your first name"/>
-                <article class="text-center p-4">
-                <button class="btn btn-dark">Register</button>
-                </article>
+          <p class="heading text-dark" align="center">Register</p>
+          <input type="username" class="form-control un" id="usernameInput" name="username" placeholder="Enter username"/>
+          
+          <input type="password" class="form-control pass" id="passwordInput" name="password" placeholder="Enter password"/>
 
-                <article class="text-center">
-                <a href="/login" class='p-3'>Login?</a>
-                <br/>
-                <a href="/" class='p-2'>Home</a>
-                </article>
-            </section>
-        </form>
-    </main>
+          <input type="text" class="form-control un" id="firstNameInput" name="firstName" placeholder="Enter your first name"/>
+
+          <article class="text-center p-4">
+            <button class="btn btn-dark">Register</button>
+          </article>
+
+          <article class="text-center">
+            <a href="/login" class='p-3'>Login?</a>
+
+            <br/>
+            
+            <a href="/" class='p-2'>Home</a>
+          </article>
+        </section>
+      </form>
+    </div>
   );
 }
 
