db39e057f429d860dabe5abcbb2bc7cc00a1fe5b
Ashley Garcia <32247665+ashgarc20@users.noreply.github.com>
2021-10-07 09:12:46 -0700

    Updated the homepage layout

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,16 +1,56 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="footer">
-      <footer class="py-1 bg-dark fixed-bottom">
-        <div class="container">
-          <p class="m-0 text-center text-white">
-            Copyright &copy; Alexis, Steven, Michael, Ashley
-          </p>
+    <section className="section footer bg-dark text-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <h6>Site Information</h6>
+            <hr/>
+            <p className="text-white">
+            Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. 
+            Duis leo. Sed fringilla mauris sit amet nibh. 
+            </p>
+          </div>
+          <div className="col-md-4">
+            <h6>Quick Links</h6>
+            <hr/>
+            <div><Link to="/">Home</Link></div>
+            <div><Link to="/register">Sign Up</Link></div>
+            <div><Link to="/login">Login</Link></div>
+          </div>
+          <div className="col-md-4">
+            <h6>Contact Information</h6>
+            <hr/>
+            <div><p className="text-white mb-2"> 123 Main Street Salinas CA </p></div>
+            <div><p className="text-white mb-1"> Some Text 1 </p></div>
+            <div><p className="text-white mb-1"> Some Text 2 </p></div>
+            <div><p className="text-white mb-1"> Some Text 3 </p></div>
+          </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+    // <div className="footer">
+    //   <footer class="py-1 bg-dark fixed-bottom">
+    //     <div class="container">
+    //       <p class="m-0 text-center text-white">
+    //         Copyright &copy; Alexis, Steven, Michael, Ashley
+    //       </p>
+    //     </div>
+    //   </footer>
+    // </div>
   );
 }
 
