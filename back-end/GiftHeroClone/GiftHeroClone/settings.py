d77ac95778d33945dc4aadc555c681663f43fac6
Steven Schreiber <53790515+StevenRS00@users.noreply.github.com>
2021-10-06 22:09:14 -0700

    Updated settings.py again
    This change allows for views to be seen without causing an error.
    
    Also updated paths for now

--- back-end/GiftHeroClone/GiftHeroClone/settings.py
+++ back-end/GiftHeroClone/GiftHeroClone/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
