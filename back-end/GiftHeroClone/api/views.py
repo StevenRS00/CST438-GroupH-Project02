b86e02c4c811399d567530670c196e5be029b3c3
Steven Schreiber <53790515+StevenRS00@users.noreply.github.com>
2021-10-06 21:40:08 -0700

    Created serializers
    Updated urls and created views as well

--- back-end/GiftHeroClone/api/views.py
+++ back-end/GiftHeroClone/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ItemSerializer, UserSerializer, ItemListSerializer
+from .models import Item, User, ItemList
 
 # Create your views here.
+
+class ItemView(generics.CreateAPIView):
+    queryset = Item.obects.all()
+    serializer_class = ItemSerializer
+
+class UserView(generics.CreateAPIView):
+    queryset = User.obects.all()
+    serializer_class = UserSerializer
+
+class ItemListView(generics.CreateAPIView):
+    queryset = ItemList.obects.all()
+    serializer_class = ItemListSerializer
