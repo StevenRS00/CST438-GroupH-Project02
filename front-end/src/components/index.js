6049f75af60525338d19a2e458b538f909929c8a
Alexis Sanchez <53790532+jwlexi@users.noreply.github.com>
2021-10-07 11:13:48 -0700

    Edit user profile page (#31)
    * Creating files...
    
    * making background blue
    
    * Finishing layout of page
    
    * Adding user's profile picture
    
    * Finishing touches
    
    * Refactoring

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -2,4 +2,5 @@ export { default as Navigation } from "./Navigation";
 export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as Login } from "./Login";
+export { default as editUserProfile } from "./editUserProfile";
 export { default as Register } from "./Register";
