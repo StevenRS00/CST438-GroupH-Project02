349647b9fcda2618aea3cf78c59ed86a17d88b60
Steven Schreiber <53790515+StevenRS00@users.noreply.github.com>
2021-10-08 20:23:10 -0700

    Update settings.py

--- back-end/GiftHeroClone/GiftHeroClone/settings.py
+++ back-end/GiftHeroClone/GiftHeroClone/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
