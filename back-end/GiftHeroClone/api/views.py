90d3841aa8de7b8fc7780f79807cccb51ab1ae93
tgmike <tgmike77@gmail.com>
2021-10-14 05:27:31 -0700

    Fiexed all existing endpoints and added 2 new endpoints. Moved manage.py

--- back-end/GiftHeroClone/api/views.py
+++ back-end/GiftHeroClone/api/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import ItemSerializer, UserSerializer, ItemListSerializer
 from .models import Item, User, ItemList
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
 class ItemView(generics.CreateAPIView):
-    queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    def get(self, request, *args, **kwargs):
+        queryset = Item.objects.all()
+        return Response(queryset, status=200)
+    
+class SpecificItemView(generics.CreateAPIView):
+    serializer_class = ItemSerializer
+    def get(self, request, id=0):
+        try:
+            queryset = Item.objects.get(id=id)
+            return Response(queryset, status=200)
+        except Item.DoesNotExist:
+            return Response(status=404)
+
+class DeleteItem(APIView):
+    serializer_class = ItemSerializer
+    def post(self, request, id=0):
+        try:
+            queryset = Item.objects.get(id=id)
+            queryset.delete()
+            return Response(status=200)
+        except Item.DoesNotExist:
+            return Response(status=404)            
 
 class UserView(generics.CreateAPIView):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+    def get(self, request, *args, **kwargs):
+        queryset = User.objects.all()
+        return Response(queryset, status=200)
 
 class ItemListView(generics.CreateAPIView):
-    queryset = ItemList.objects.all()
     serializer_class = ItemListSerializer
-    
+    def get(self, request, *args, **kwargs):
+        queryset = ItemList.objects.all()
+        return Response(queryset, status=200)
