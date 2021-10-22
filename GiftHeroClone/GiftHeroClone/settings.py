73879419941b663b421dd14e313fb9b2175b721a
Steven Schreiber <53790515+StevenRS00@users.noreply.github.com>
2021-10-22 16:53:30 -0700

    Got CSS working
    Routes are a little scuffed

--- GiftHeroClone/GiftHeroClone/settings.py
+++ GiftHeroClone/GiftHeroClone/settings.py
@@ -64,7 +64,9 @@ ROOT_URLCONF = 'GiftHeroClone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'front-end/build'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,13 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'front-end/build/static'),
 )
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-django_heroku.settings(locals())
--- GiftHeroClone/GiftHeroClone/urls.py
+++ GiftHeroClone/GiftHeroClone/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from django.conf import settings
-from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
-
-static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
