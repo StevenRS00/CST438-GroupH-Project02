c8a851de5ef3c04d7d347bccf411d889c3a48315
tgmike <tgmike77@gmail.com>
2021-10-23 13:07:30 -0700

    Most routes are now up and working

--- back-end/GiftHeroClone/api/models.py
+++ back-end/GiftHeroClone/api/models.py
@@ -1,18 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Item(models.Model):
-    item_name = models.CharField(max_length=50)
-    description = models.TextField(max_length=250)
+    item_name = models.CharField(blank=False, max_length=50)
+    description = models.TextField(blank=True, max_length=250)
     image_url = models.URLField(blank=True, max_length=100)
     item_url = models.URLField(blank=True, max_length=100)
 
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=30)
+class User(AbstractUser):
     fullname = models.CharField(max_length=50)
     is_admin = models.BooleanField(default=False)
 
 class ItemList(models.Model):
-    items = models.ManyToManyField(Item)
-    users = models.ManyToManyField(User)
+    items = models.ManyToManyField(Item, blank=True)
+    users = models.ManyToManyField(User, blank=True)
--- back-end/GiftHeroClone/api/views.py
+++ back-end/GiftHeroClone/api/views.py
@@ -1,11 +1,31 @@
+from django.db.models import query
 from django.shortcuts import render
 from rest_framework import generics, status
 from .serializers import ItemSerializer, UserSerializer, ItemListSerializer
 from .models import Item, User, ItemList
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import update_session_auth_hash
 # Create your views here.
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({"Success": "User is logged in"}, status=200)
+        else:
+            return Response({"Error": "No user found"}, status=401)
+
+class LogoutView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    def post(self, request):
+        logout(request)
+        return Response(status=200)
 
 class AllItemView(generics.CreateAPIView):
     serializer_class = ItemSerializer
@@ -27,7 +47,7 @@ class SpecificItemView(generics.CreateAPIView):
 
 class DeleteItemView(APIView):
     serializer_class = ItemSerializer
-    def post(self, request, id=0):
+    def delete(self, request, id=0):
         try:
             queryset = Item.objects.get(id=id)
             queryset.delete()
@@ -48,13 +68,32 @@ class CreateItemView(APIView):
             id = serializer.data.get('id')
             queryset = Item.objects.filter(id=id)
             if queryset.exists():
-                return Response({'Content': 'Account already exists'}, status=403);
+                return Response({'Content': 'Account already exists'}, status=403)
             else:
                 item = Item(item_name=item_name, description=description,
                             image_url=image_url, item_url=item_url)
                 item.save()
                 return Response(ItemSerializer(item).data, status=status.HTTP_201_CREATED)
-        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)            
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+class UpdateItemView(APIView):
+    serializer_class = ItemSerializer
+    def patch(self, request, format=None, id=0):
+        item_name = request.data['item_name']
+        description = request.data['description']
+        image_url = request.data['image_url']
+        item_url = request.data['item_url']
+        queryset = Item.objects.filter(id=id)
+        if queryset.exists():
+            item = Item.objects.get(id=id)
+            item.item_name = item_name
+            item.description = description
+            item.image_url = image_url
+            item.item_url = item_url
+            item.save()
+            return Response({'Success': 'Item successfully updated'}, status=200)
+        else:
+            return Response({'Does Not Exist': 'Item does not exist'}, status=404)            
 
 class AllUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
@@ -70,13 +109,14 @@ class SpecificUserView(generics.CreateAPIView):
         try:
             queryset = User.objects.get(id=id)
             data = UserSerializer(queryset).data
+            print(data)
             return Response(data, status=200)
         except User.DoesNotExist:
             return Response(status=404)
 
 class DeleteUserView(APIView):
     serializer_class = UserSerializer
-    def post(self, request, id=0):
+    def delete(self, request, id=0):
         try:
             queryset = User.objects.get(id=id)
             queryset.delete()
@@ -94,13 +134,32 @@ class CreateUserView(APIView):
             fullname = serializer.data.get('fullname')
             queryset = User.objects.filter(username=username)
             if queryset.exists():
-                return Response({'Content': 'Account already exists'}, status=403);
+                return Response({'Content': 'Account already exists'}, status=403)
             else:
-                user = User(username=username, password=password,
+                user = User.objects.create_user(username=username, password=password,
                             fullname=fullname)
                 user.save()
                 return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)  
+
+class UpdateUserView(APIView):
+    serializer_class = UserSerializer
+    def patch(self, request, format=None):
+        id=request.user.id
+        username = request.data['username']
+        password = request.data['password']
+        fullname = request.data['fullname']
+        queryset = User.objects.filter(id=id)
+        if queryset.exists():
+            user = User.objects.get(id=id)
+            user.username = username
+            user.set_password(password)
+            user.fullname = fullname
+            user.save()
+            update_session_auth_hash(request, user)
+            return Response({'Success': 'Account successfully updated'}, status=200)
+        else:
+            return Response({'Does Not Exist': 'User does not exist'}, status=404)
 
 class AllItemListView(generics.CreateAPIView):
     serializer_class = ItemListSerializer
@@ -122,10 +181,24 @@ class SpecificItemListView(generics.CreateAPIView):
 
 class DeleteItemListView(APIView):
     serializer_class = ItemListSerializer
-    def post(self, request, id=0):
+    def delete(self, request, id=0):
         try:
             queryset = ItemList.objects.get(id=id)
             queryset.delete()
             return Response(status=200)
         except ItemList.DoesNotExist:
             return Response(status=404) 
+
+class CreateItemListView(APIView):
+    serializer_class = ItemListSerializer
+    user_serializer_class = UserSerializer
+    def post(self, request, format=None):  
+        try:
+            id = request.user.id
+            user = User.objects.get(id=id)
+            item_list = ItemList.objects.create()
+            item_list.users.add(user)
+            item_list.save()
+            return Response(ItemListSerializer(item_list).data, status=status.HTTP_201_CREATED)
+        except User.DoesNotExist:
+            return Response({'Does Not Exist': 'User does not exist'},status=404) 
